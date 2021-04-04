c1e27eb1b5394f17f42dec9068b1ab04c75f5dfc
ciamac-da <ciamac.davoudi@gmail.com>
2021-04-04 12:34:18 +0200

    Verified by Marti4n v1.0.9 Adding the build Control Component

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import myStyles from "./BuildControlStyle"
+
+
+const BuildControl = (props) => {
+
+    const classes = myStyles();
+  return (
+    <div className={classes.BuildControl}>
+      <div className={classes.Label}>{props.label} </div>
+      <Button className={classes.More} variant="outlined" color="secondary">
+        Less
+      </Button>
+      <Button className={classes.Less} variant="outlined" color="secondary">
+        More
+      </Button>
+    </div>
+  );
+};
+
+export default BuildControl;
---
+++ src/components/Burger/BuildControls/BuildControlsStyle.js
@@ -0,0 +1,16 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme)=>({
+    BuildControls:{
+        margin:"auto",
+        width:"100%",
+        display:"flex",
+        flexFlow:"column",
+        alignItems:"center",
+        boxShadow:"0 2px 1px white",
+        padding:"10px 0"
+    }
+  }));
+
+  export default useStyles;
