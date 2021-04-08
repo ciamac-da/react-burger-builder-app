45452b723997c70007284b523136255c4a1014d3
ciamac-da <ciamac.davoudi@gmail.com>
2021-04-08 19:28:53 +0200

    Verified by Marti4n v1.3.6 sending a post request

--- src/components/BurgerBulder/BurgerBuilder.js
+++ src/components/BurgerBulder/BurgerBuilder.js
@@ -3,7 +3,8 @@ import Aux from "../../hoc/aux";
 import Burger from "../Burger/Burger";
 import BurgerControls from "../Burger/BuildControls/BuildControls";
 import Modal from "../UI/Modal/Modal";
-import OrderSummary from '../Burger/OrderSummary/OrderSummary';
+import OrderSummary from "../Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
   salad: 1.5,
@@ -22,21 +23,21 @@ class BurgerBuilder extends Component {
         cheese: 0,
         meat: 0,
       },
-     totalPrice:3,
-     purchaseable: false,
-     purchasing: false
+      totalPrice: 3,
+      purchaseable: false,
+      purchasing: false,
     };
   }
 
   updatePurchaseState(ingredients) {
     const sum = Object.keys(ingredients)
-    .map(igKey =>{
-      return ingredients[igKey]
-    })
-    .reduce((sum, el)=>{
-      return sum + el
-    },0);
-    this.setState({purchaseable:sum >0})
+      .map((igKey) => {
+        return ingredients[igKey];
+      })
+      .reduce((sum, el) => {
+        return sum + el;
+      }, 0);
+    this.setState({ purchaseable: sum > 0 });
   }
 
   addIngredientHandler = (type) => {
@@ -46,51 +47,71 @@ class BurgerBuilder extends Component {
       ...this.state.ingredients,
     };
     updatedIngredients[type] = updatedCount;
-    const priceAddition = INGREDIENT_PRICES[type]
+    const priceAddition = INGREDIENT_PRICES[type];
     const oldPrice = this.state.totalPrice;
-    const newPrice = oldPrice +priceAddition
-    this.setState({ingredients: updatedIngredients, totalPrice: newPrice })
-    this.updatePurchaseState(updatedIngredients)
+    const newPrice = oldPrice + priceAddition;
+    this.setState({ ingredients: updatedIngredients, totalPrice: newPrice });
+    this.updatePurchaseState(updatedIngredients);
   };
 
   removeIngredientHandler = (type) => {
     const oldCount = this.state.ingredients[type];
-    if(oldCount <= 0){
-        return;
+    if (oldCount <= 0) {
+      return;
     }
-    const updatedCount = oldCount -1;
+    const updatedCount = oldCount - 1;
     const updatedIngredients = {
       ...this.state.ingredients,
     };
     updatedIngredients[type] = updatedCount;
-    const priceDeduction = INGREDIENT_PRICES[type]
+    const priceDeduction = INGREDIENT_PRICES[type];
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice - priceDeduction;
-    this.setState({ingredients: updatedIngredients, totalPrice: newPrice })
-    this.updatePurchaseState(updatedIngredients)
+    this.setState({ ingredients: updatedIngredients, totalPrice: newPrice });
+    this.updatePurchaseState(updatedIngredients);
   };
 
   purchasingHandler = () => {
-    this.setState({purchasing:true})
-  }
+    this.setState({ purchasing: true });
+  };
 
   purchasingCancelHandler = () => {
-    this.setState({purchasing:false})
-  }
-  purchasingContinueHandler = () =>{
-    console.log("Just Continue")
-  }
+    this.setState({ purchasing: false });
+  };
+  purchasingContinueHandler = () => {
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      costumer: {
+        name: "Cia",
+        address: {
+          street: "Earth",
+          zipCode: "41678",
+          country: "Italy",
+        },
+        email: "tjah@tjah.com",
+      },
+      deliveryMethod:"Fastest"
+    };
+    axios.post("/orders.json", order)
+    .then(response=>{
+      console.log(response)
+    })
+    .catch(error=>{
+      console.log(error)
+    })
+  };
 
   render() {
-  let disabledInfo = {
-      ...this.state.ingredients
-  }
+    let disabledInfo = {
+      ...this.state.ingredients,
+    };
 
-  for (let key in disabledInfo){
-      disabledInfo[key] = disabledInfo[key] <= 0 
-  }
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
 
-/*   let disabledMaxInfo = {
+    /*   let disabledMaxInfo = {
     ...this.state.ingredients
 }
 
@@ -99,26 +120,26 @@ for (let keys in disabledMaxInfo){
 } */
     return (
       <Aux>
-        <Modal 
-        show={this.state.purchasing} 
-        modalClosed={this.purchasingCancelHandler}
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchasingCancelHandler}
         >
-          <OrderSummary 
-          ingredients={this.state.ingredients} 
-          purchaseCancelled={this.purchasingCancelHandler}  
-          purchaseContinued={this.purchasingContinueHandler}  
-          price={this.state.totalPrice}
+          <OrderSummary
+            ingredients={this.state.ingredients}
+            purchaseCancelled={this.purchasingCancelHandler}
+            purchaseContinued={this.purchasingContinueHandler}
+            price={this.state.totalPrice}
           />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
-        <BurgerControls 
-        ingredientAdded={this.addIngredientHandler} 
-        ingredientRemoved={this.removeIngredientHandler} 
-        disabled={disabledInfo}
-        purchaseable={this.state.purchaseable}
-        //disabledMax={disabledMaxInfo}
-        price={this.state.totalPrice}
-        ordered={this.purchasingHandler}
+        <BurgerControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo}
+          purchaseable={this.state.purchaseable}
+          //disabledMax={disabledMaxInfo}
+          price={this.state.totalPrice}
+          ordered={this.purchasingHandler}
         />
       </Aux>
     );
